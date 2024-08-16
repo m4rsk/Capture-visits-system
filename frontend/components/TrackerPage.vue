9fb4289ffcc4cf2de5959910af9d860aa3a579d5
m4rsk <165821083+m4rsk@users.noreply.github.com>
2024-08-16 23:18:07 +0300

    Create TrackerPage.vue

---
+++ frontend/components/TrackerPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrackerPage',
+  mounted() {
+    (function (d, w) {
+      var n = d.getElementsByTagName("script")[0],
+      s = d.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "http://188.225.24.191/pixel.js?ref=" + d.referrer + "&page=" + encodeURIComponent(w.location.href);
+      n.parentNode.insertBefore(s, n);
+    })(document, window);
+  }
+};
+</script>
